44e9413f367a7442a19e19983aaa014fcc3b3cb1
Edgar Pacheco <nielpaxeko@gmail.com>
2024-11-14 19:32:54 -0500

    added linguistic skills to portfolio

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -1,16 +1,36 @@
-import { Image } from 'react-bootstrap';
+import { Image, Row, Col } from 'react-bootstrap';
 import { useEffect, useState, useMemo, useRef } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Skill Icons
 import HTML from '../assets/images/html-logo.png';
 import CSS from '../assets/images/css-logo.png';
 import Javascript from '../assets/images/javascript-logo.png';
-import NodeJS from '../assets/images/node.png'
-import React from '../assets/images/react.png'
+import NodeJS from '../assets/images/node.png';
+import ReactImg from '../assets/images/react.png';
 import Python from '../assets/images/python-logo.png';
 import Java from '../assets/images/java-logo.png';
 import SQL from '../assets/images/sql-logo.png';
 import spin from '../assets/images/spin-me.webp';
+// Language Icons
+import spanish from '../assets/images/languages/spanish.png';
+import english from '../assets/images/languages/english.png';
+import italian from '../assets/images/languages/italian.png';
+import japanese from '../assets/images/languages/japanese.png';
+import french from '../assets/images/languages/french.png';
+import german from '../assets/images/languages/german.png';
+// Nation Icons
+import mondstadt from '../assets/images/languages/mondstadt.png';
+
+// Languages with properties: name, color, proficiency
+const languages = [
+  { name: 'Spanish', color: '#C70039', icon: spanish, nation: mondstadt, proficiency: 5 },
+  { name: 'English', color: '#3A7D44', icon: english, proficiency: 5 },
+  { name: 'Italian', color: '#6D9EEB', icon: italian, proficiency: 4 },
+  { name: 'Japanese', color: '#9479C0', icon: japanese, proficiency: 3.5 },
+  { name: 'French', color: '#CE3737', icon: french, proficiency: 4 },
+  { name: 'German', color: '#28B463', icon: german, proficiency: 2.5 },
+];
 
 export const Skills = () => {
   const skills = useMemo(
@@ -19,7 +39,7 @@ export const Skills = () => {
       { name: 'CSS', image: CSS, color: '#264DE4' },
       { name: 'Javascript', image: Javascript, color: '#F0DB4F' },
       { name: 'NodeJS', image: NodeJS, color: '#53A040' },
-      { name: 'React', image: React, color: '#61DBFB' },
+      { name: 'React', image: ReactImg, color: '#61DBFB' },
       { name: 'Python', image: Python, color: '#2596be' },
       { name: 'Java', image: Java, color: '#CE3737' },
       { name: 'SQL', image: SQL, color: '#BE63F9' },
@@ -57,7 +77,6 @@ export const Skills = () => {
 
     animate();
 
-    // Event listeners
     const handleMouseOver = (index) => {
       setIsPaused(true);
       setHoveredSkill({ name: skills[index].name, color: skills[index].color });
@@ -81,31 +100,65 @@ export const Skills = () => {
       icon.addEventListener('mouseout', handleMouseOut);
     });
 
-    // Cleanup
     return () => {
       if (animationRef.current) {
         cancelAnimationFrame(animationRef.current);
       }
-      skillIcons.forEach((icon, index) => {
-        icon.removeEventListener('mouseover', () => handleMouseOver(index));
+      skillIcons.forEach((icon) => {
+        icon.removeEventListener('mouseover', handleMouseOver);
         icon.removeEventListener('mouseout', handleMouseOut);
       });
     };
   }, [isPaused, skills]);
 
-  return (
-    <section id='skills'>
-      <h2 className='text-center mb-5 section-title red-text'>My Skills</h2>
-      <div className="skill-wheel">
-        {skills.map((skill, index) => (
-          <div className="skill-icon" key={index}>
-            <Image src={skill.image} alt={skill.name} />
-          </div>
-        ))}
-        <div className="skill-center" style={{ color: hoveredSkill.color }}>
-          {hoveredSkill.name ? hoveredSkill.name :   <img src={spin} alt="Skill Animation" style={{ width: '100px', height: '100px' }} />}
-        </div>
+  // Bookshelf Component
+  const renderBookshelf = () => {
+    return (
+      <div className="bookshelf">
+        {languages.map((language, index) => {
+          const bookStyle = {
+            backgroundColor: language.color,
+            height: `${language.proficiency * 50}px`,
+            transform: language.name === 'Spanish' ? 'translate(-16px, -2.5px) rotate(+7.5deg)' : 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            writingMode: 'vertical-lr',
+          }; 
+
+          return (
+            <div className="book" key={index} style={bookStyle}>
+              {language.name}
+              <img src={language.icon} alt={`${language.name} icon`} style={{  marginTop: '4px', width: '30px', height: '30px' }} />
+            </div>
+          );
+        })}
       </div>
+    );
+  };
+
+  return (
+    <section id="skills">
+      <h2 className="text-center mb-5 section-title red-text">My Skills</h2>
+      <Row>
+        <Col lg={6} className="technical-skills mb-lg-0">
+          <div className="skill-wheel">
+            {skills.map((skill, index) => (
+              <div className="skill-icon" key={index}>
+                <Image src={skill.image} alt={skill.name} />
+              </div>
+            ))}
+            <div className="skill-center" style={{ color: hoveredSkill.color }}>
+              {hoveredSkill.name ? hoveredSkill.name : (
+                <img src={spin} alt="Skill Animation" style={{ width: '100px', height: '100px' }} />
+              )}
+            </div>
+          </div>
+        </Col>
+        <Col lg={6} className='linguistic-skills mt-lg-0'>
+          {renderBookshelf()}
+        </Col>
+      </Row>
     </section>
   );
 };
